481b80546b2df4aa9dc8fe4b2c05291167cd5000
AidenParkerr <aidenparker1998@live.co.uk>
2023-06-01 23:34:43 +0100

    Formatted code to PEP8 standards.

--- temperature_notifier.py
+++ temperature_notifier.py
@@ -7,8 +7,10 @@ import signal
 import fcntl
 import argparse
 
+
 class TempMonitor():
-    def __init__(self, config_path: str, device_name: str, temp_thresholds: dict=None) -> None:
+    def __init__(self, config_path: str, device_name: str,
+                 temp_thresholds: dict = None) -> None:
         self.temp_thresholds = temp_thresholds
         self.config_path = config_path
         self.device_name = device_name
@@ -19,32 +21,36 @@ class TempMonitor():
         signal.signal(signal.SIGTERM, self._signal_handler)
         if self.temp_thresholds is None:
             self.temp_thresholds = {
-            80.0: "TEMPERATURE IS VERY HIGH",
-            70.0: "TEMPERATURE IS HIGH",
-            60.0: "TEMPERATURE IS CLIMBING. Keep an eye on it",
-            }   
-       
-       
+                80.0: "TEMPERATURE IS VERY HIGH",
+                70.0: "TEMPERATURE IS HIGH",
+                60.0: "TEMPERATURE IS CLIMBING. Keep an eye on it",
+            }
+
     def _read_config(self):
         config = configparser.ConfigParser()
         config.read(self.config_path)
 
-        if not config.has_section('Telegram') or "bot_token" not in config['Telegram'] or "chat_id" not in config['Telegram']:
+        if not config.has_section(
+                'Telegram') or "bot_token" not in config['Telegram'] or "chat_id" not in config['Telegram']:
             self.logger.critical(
                 "Telegram bot token or chat ID not found in config file. Exiting...")
-            
-            self.logger.info(f"Read from config.ini: `{config.read(self.config_path)}`")
-            raise ValueError("Telegram bot token or chat ID not found in config.ini file. This could be due to the format of the config file.")
 
-        
+            self.logger.info(
+                f"Read from config.ini: `{config.read(self.config_path)}`")
+            raise ValueError(
+                "Telegram bot token or chat ID not found in config.ini file. This could be due to the format of the config file.")
+
         self.bot_token = config['Telegram']['bot_token'].strip('"')
         self.chat_id = config['Telegram']['chat_id'].strip('"')
-           
+
     def _init_logger(self) -> None:
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
-	
-        log_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'temps.log')
+
+        log_path = os.path.join(
+            os.path.dirname(
+                os.path.abspath(__file__)),
+            'temps.log')
         file_handler = logging.FileHandler(log_path)
         console_handler = logging.StreamHandler()
 
@@ -61,7 +67,6 @@ class TempMonitor():
         self.logger.addHandler(file_handler)
         self.logger.addHandler(console_handler)
 
-    
     def _signal_handler(self, signum, frame) -> None:
         if signum == signal.SIGINT:
             self.logger.info("Received SIGINT signal. Exiting...")
@@ -95,28 +100,32 @@ class TempMonitor():
             self.logger.error(
                 f"Failed to send notification to Telegram: {e}",
                 exc_info=True)
-       
+
     def get_temp(self) -> float:
         self.logger.info("Retriving temperature...")
-        try: 
+        try:
             temp = os.popen("vcgencmd measure_temp").readline()
-            temp_float_val: float = float(temp.replace("temp=","").replace("'C\n",""))
+            temp_float_val: float = float(
+                temp.replace(
+                    "temp=", "").replace(
+                    "'C\n", ""))
         except Exception as e:
             self.logger.error(f"Failed to get temperature: {e}", exc_info=True)
             return None
 
         self.logger.info(f"Temperature Recieved. Temp: {temp_float_val}c.")
         return temp_float_val
-    
+
     def monitor_temp(self, temp: float) -> None:
         if temp is None:
             self.logger.error("Temperature is None, skipping...")
             return
-        
+
         for threshold, message in self.temp_thresholds.items():
             if temp > threshold:
                 self.logger.warning(f"Temperature exceeds {threshold}c")
-                self.send_notification(f"*WARNING - TEMPERATURE EXCEEDS {threshold}c.*\n{message}: {temp}c!")
+                self.send_notification(
+                    f"*WARNING - TEMPERATURE EXCEEDS {threshold}c.*\n{message}: {temp}c!")
                 break
 
     def run(self) -> None:
@@ -127,17 +136,16 @@ class TempMonitor():
             self.logger.error(
                 f"Failed to create lock file: {e}", exc_info=True)
             return
-        
+
         try:
             fcntl.lockf(lock_file_descriptor, fcntl.LOCK_EX | fcntl.LOCK_NB)
         except IOError as e:
             self.logger.error(
                 f"An Instance of this script is already running: {e}. Exiting...")
             return
-        
-        
-        try:    
-            temp: float = self.get_temp()            
+
+        try:
+            temp: float = self.get_temp()
             # Wait for 5 seconds before checking again if `temp`` is None.
             if temp is None:
                 time.sleep(5)
@@ -152,7 +160,7 @@ class TempMonitor():
         except Exception as e:
             self.logger.error(f"Unexpected error: {e}")
             self.send_notification(
-                    f"Temperature Notifier experienced an unexpected erro:r: {e}")
+                f"Temperature Notifier experienced an unexpected erro:r: {e}")
         finally:
             # Release the lock on the lock file and delete it.
             self.logger.info("Removing lock file...")
@@ -164,6 +172,7 @@ class TempMonitor():
                 self.logger.error(
                     f"Failed to remove lock file: {e}",
                     exc_info=True)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Temperature Monitor")
@@ -178,8 +187,9 @@ if __name__ == '__main__':
         default="device1",
         help="Device name")
 
-
     args = parser.parse_args()
 
-    temp_monitor = TempMonitor(config_path=args.config_file, device_name=args.device_name)
+    temp_monitor = TempMonitor(
+        config_path=args.config_file,
+        device_name=args.device_name)
     temp_monitor.run()
